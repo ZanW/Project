cb817a4ffe73e3f2cf620cde5d8a0758f5265371
xdwang0726 <starwxd@gmail.com>
2016-07-18 22:20:18 -0400

    update content controller

--- application/models/Content_model.php
+++ application/models/Content_model.php
@@ -19,4 +19,35 @@ class Content_model extends CI_Model
        $querie = $this->db->get('content');
         return $querie->result_array();
     }
+
+    public function get_content_by_id($id = 0)
+	{
+		$query = $this->db->get_where('content', array('id' => $id));
+		return $query->result_array();
+	}
+
+	public function set_content()
+	{
+		$data = array(
+			'post_message' => $this->input->post('post_message'),);
+
+		return $this->db->insert('content', $data);
+	}
+
+	public function update_content()
+	{
+		$data = array(
+			'post_message' => $this->input->post('post_message'),);
+
+		$this->db->where('id', $this->input->post('id'));
+		
+		return $this->db->update('content', $data);
+	}
+
+	public function delete_content()
+	{
+		$data = array( 'id' => $this->input->post('id') );
+
+		return $this->db->delete('content', $data);
+	}
 }
