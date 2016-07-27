1525f9cd7b0051a68870e2cff8d401a33acfd1b2
Jaya Gautam <tannu.gautam19@gmail.com>
2016-07-26 20:24:04 -0600

    login_registarion_validation with member profile edit and update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'https://ioc55311.encs.concordia.ca/pog/';
-$config['base_url'] = 'http://localhost/pog/';
+$config['base_url'] = 'http://localhost:8080/pog/';
 
 /*
 |--------------------------------------------------------------------------
