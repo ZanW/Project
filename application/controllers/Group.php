b9b425e9fd782117ebe44bb6fadb30b404f188ae
Shivam <shivam.nigam@ericsson.com>
2016-08-13 21:04:25 -0400

    Feature: Leave a group

--- application/controllers/Group.php
+++ application/controllers/Group.php
@@ -26,6 +26,7 @@ class Group extends CI_Controller
         $data['data_get'] =     $this->group_crud->view ($mid) ;
         $data['data_get_all'] = $this->group_crud->viewAllGroupsOfMember($mid);
      //   $data_get_all = $this->group_crud->viewAllGroupsOfMember();
+        $data['data_get_other_group']=$this->group_crud->otherGroupsOfMember($mid);
         $this->load->view ( 'group/group_header', $data ) ;
         $this->load->view ('templates/header.php');
         $this->load->view ( 'group/group_view', $data ) ;
@@ -55,7 +56,21 @@ class Group extends CI_Controller
         redirect("group/index/".$_SESSION["ID"]);
     }
     
-    // TODO memberID
+    /**
+     * this function removes the member from  a group . It will remove the member_id and Group_id from
+     * table gm_member_of
+     * Precondition: 1. member should not be the owner of the group
+     *               2. member must be member of the group
+     *               
+     * @param member_id and group_id
+     */
+    public function leaveGroup($g_id,$m_id)
+    {
+        //echo "group_id and member_id=".$g_id."  ,  ".$m_id;
+        $this->group_crud->deleteMemberFromGroup($g_id,$m_id) ;
+        redirect("group/index/".$_SESSION["ID"]);
+    }
+    
     public function add()
     
     {
